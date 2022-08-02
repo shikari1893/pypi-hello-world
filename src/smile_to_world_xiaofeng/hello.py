dc78eda33baf8182c5d8f9b5dc38dbe6577b1e31
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2022-08-02 19:32:58 +0530

    Update hello.py

--- src/smile_to_world_xiaofeng/hello.py
+++ src/smile_to_world_xiaofeng/hello.py
@@ -2,6 +2,6 @@ import time
 
 class Hello:
 	def __init__(self):
-		self._author = 'Xiaofeng Zhu'
+		self._author = 'Shiv Shakti'
 	def print_message(self):
 		print('Hello world from {} at {}'.format(self._author, time.time()))
